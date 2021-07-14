7beb1c742a437b4d8d6f05007f1ade2a9cbd9141
CaiyuTing <caiyuting@agora.io>
2021-07-14 16:43:56 +0800

    new

---
+++ Ch02-di-xml/src/main/java/org/example/student.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+public class student {
+
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
